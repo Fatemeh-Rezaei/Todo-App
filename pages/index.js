648468592b9ea41c8a46683adcf797926e8dca70
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-03 21:50:54 +0430

    Restrict home page access and Finished :)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 import HomePage from "@/templates/HomePage";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
   return <HomePage />;
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
